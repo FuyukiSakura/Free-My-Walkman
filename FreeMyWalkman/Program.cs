f283543bc7c92329fb5202c3a2b025db37250d41
Eric Lam <ericlamyauping@gmail.com>
2017-11-26 21:01:36 +1000

    read directory if playlist found

--- FreeMyWalkman/Program.cs
+++ FreeMyWalkman/Program.cs
@@ -38,6 +38,42 @@ namespace FreeMyWalkman
                 }
             }
 
+            //Load directories
+            foreach(var targetDirectory in folders)
+            {
+                var playlists = Directory.GetFiles(targetDirectory, "*.M3U8");
+                if (playlists.Any())
+                {
+                    ProcessDirectory(targetDirectory);
+                }
+                else
+                {
+                    //No Playlist in folder - Do NOT process
+                    Console.WriteLine("No Walkman playlist found in directory. Please make sure you are selecting the root folder of your Walkman.");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        // Process all files in the directory passed in, recurse on any directories 
+        // that are found, and process the files they contain.
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        // Insert logic for processing found files here.
+        public static void ProcessFile(string path)
+        {
+            Console.WriteLine("Processed file '{0}'.", path);
         }
     }
 }
