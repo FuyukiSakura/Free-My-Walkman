f555af68fa59bec73dfb1960cca9b4227e46e2b7
Eric Lam <ericlamyauping@gmail.com>
2017-11-26 23:30:40 +1000

    script to build binary for release

---
+++ FreeMyWalkman/release.cmd
@@ -0,0 +1,8 @@
+:: Build Versions
+:: Windows 10
+:: Ubuntu
+:: OSX 
+dotnet publish -c Release -r win10-x64
+dotnet publish -c Release -r ubuntu.16.10-x64
+dotnet publish -c Release -r osx-x64
+pause
