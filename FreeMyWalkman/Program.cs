b0b54bc3226595071164a0795da101a384e43899
Eric Lam <ericlamyauping@gmail.com>
2017-11-26 21:00:37 +1000

    Load arguments

--- FreeMyWalkman/Program.cs
+++ FreeMyWalkman/Program.cs
@@ -15,6 +15,29 @@ namespace FreeMyWalkman
                 Console.WriteLine("You need to provide at least a directory as argument.");
                 return;
             }
+
+            //Add Paths
+            var folders = new List<string>();
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith("-"))
+                {
+
+                }
+                else
+                {
+                    if (Directory.Exists(arg))
+                    {
+                        //Add path only if exists
+                        folders.Add(arg);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Directory: `{arg}` is not found.");
+                    }
+                }
+            }
+
         }
     }
 }
